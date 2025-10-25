6aea1abad71c74331cca02e67d6ba7d3ce22f244
Your Name <you@example.com>
2025-10-24 21:39:23 -0500

    Login interface

---
+++ src/screens/Login.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import {  
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert
+} from 'react-native';
+
+function Login(){
+    return (
+        <View style={styles.container}>
+            <Text>Login session</Text>
+            <TextInput
+                style={styles.input} 
+                placeholder='joe@mail.com'
+                placeholderTextColor='#3e3d3dff'
+            />
+            <TextInput
+                style={styles.input} 
+                placeholder='*************'
+                placeholderTextColor='#3e3d3dff'
+                secureTextEntry
+            />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.btnLoginText}>Sign in</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.registerText}>
+                <Text style={styles.registerText}>
+                    I don't have an account <Text style={styles.registerLink}>Sign up</Text>
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 50,
+        paddingTop: 80,
+        paddingBottom: 40
+    },
+    input: {
+        width: "100%",
+        height: 50,
+        backgroundColor: "#fff",
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: "#ddd",
+        paddingHorizontal: 15,
+        marginBottom: 20,
+    },
+    button: {
+        width: "100%",
+        backgroundColor: "#007BFF",
+        paddingVertical: 14,
+        borderRadius: 12,
+        alignItems: "center",
+        marginTop: 10
+    },
+    btnLoginText: {
+        color: "#f4ececff",
+        fontSize: 14,
+        fontWeight: "bold"
+    },
+    registerText:{
+        color: "#292828ff",
+        marginTop: 30,
+        fontSize: 14,
+    },
+    registerLink: {
+        color: "#007BFF",
+        fontWeight: "bold"
+    }
+});
